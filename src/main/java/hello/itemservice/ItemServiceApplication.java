fb096fd08cf39587d3443904e82ce3aa15623a89
chanboklee <chanboklee@naver.com>
2023-02-08 01:51:37 +0900

    Fix : data-access skill - embedded mode database

--- src/main/java/hello/itemservice/ItemServiceApplication.java
+++ src/main/java/hello/itemservice/ItemServiceApplication.java
@@ -5,16 +5,21 @@ import hello.itemservice.repository.ItemRepository;
 import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV1;
 import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV2;
 import hello.itemservice.repository.jdbctemplate.JdbcTemplateItemRepositoryV3;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Profile;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+
+import javax.sql.DataSource;
 
 
 // @Import(MemoryConfig.class)
 // @Import(JdbcTemplateItemRepositoryV1.class)
 // @Import(JdbcTemplateItemRepositoryV2.class)
+@Slf4j
 @Import(JdbcTemplateItemRepositoryV3.class)
 @SpringBootApplication(scanBasePackages = "hello.itemservice.web")
 public class ItemServiceApplication {
@@ -29,4 +34,15 @@ public class ItemServiceApplication {
 		return new TestDataInit(itemRepository);
 	}
 
+	@Bean
+	@Profile("test")
+	public DataSource dataSource(){
+		log.info("메모리 데이터베이스 초기화");
+		DriverManagerDataSource dataSource = new DriverManagerDataSource();
+		dataSource.setDriverClassName("org.h2.Driver");
+		dataSource.setUrl("jdbc:h2:mem:db;DB_CLOSE_DELAY=-1");
+		dataSource.setUsername("sa");
+		dataSource.setPassword("");
+		return dataSource;
+	}
 }
